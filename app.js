47bea440b7ec342e2db4df3126c4ab1330bc2979
Vlada Salinikova <iastoboii@gmail.com>
2022-08-14 20:25:38 +0300

    fix: исправить ошибку после автотеста #4

--- app.js
+++ app.js
@@ -50,7 +50,8 @@ app.use((err, req, res, next) => {
   res
     .status(statusCode)
     .send({
-      message: statusCode === 500
+      statusCode,
+      error: statusCode === 500
         ? 'На сервере произошла ошибка'
         : message,
     });
